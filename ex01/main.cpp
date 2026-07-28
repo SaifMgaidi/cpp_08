ae48f0aac2d8efde27708171540caddaedc1d046
Saif <saif4547@gmail.com>
2026-07-28 17:08:09 +0200

    ex01 - add main subject test

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -80,5 +80,15 @@ int	main(void)
 	}
 	
 
-	return (0);
+
+	std::cout << "\n============= subject main test ===========================" << std::endl;
+	Span sp = Span(5);
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	std::cout << sp.shortestSpan() << std:: endl;
+	std::cout << sp.longestSpan() << std::endl;
+	return 0;
 }
